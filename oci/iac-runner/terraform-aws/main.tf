0476b7d4f72cbdaddd3e59a7e88e2b782551d254
Haitao Pan <manbuzhe2009@qq.com>
2023-02-20 16:15:15 +0800

    update .github/workflows/terraform-aws.yaml

---
+++ oci/iac-runner/terraform-aws/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = "ap-east-1"
+}
+
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.14.0"  
+    }
+    tls = {
+      source  = "hashicorp/tls"
+    }
+  }
+}
